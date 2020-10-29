3391a75a16cb252b613e61306f92b1402227ba20
Ben Cook <benjamin.cook@ucalgary.ca>
2020-10-28 18:23:15 -0600

    timestamp and history done

--- index.js
+++ index.js
@@ -9,19 +9,44 @@ app.get('/',(req,res) =>{
     res.sendFile(__dirname + '/index.html')
 });
 
-let UserID = 0;
+let userID = 0;
+let chatHistoryLimit = 200; //CHANGE TO 200 BEFORE FINAL RELEASElet chatHistory = [];
+let chatHistory = []
+
+//trims chat history according to the chat history limit
+function chatHistoryTrim(history){
+    if(history.length > chatHistoryLimit){
+        history = history.slice(history.length - chatHistoryLimit,history.length)
+    } 
+    return(history)
+}
+
 //on connect and disconnect messages
 io.on('connection', (socket)=>{    
-    UserID = UserID + 1;
-    console.log('a user connected and assigned ID ' + UserID);
+    userID = userID + 1;
+    console.log('a user connected and assigned ID ' + userID);
+    //userID and chat hisotry to emit
+    let data = {
+        userID: "User" + userID,
+        history: chatHistory
+    };
     //on chat message print to the console
-    io.emit("Joined", "User" + UserID);
+    io.emit("Joined", data);
     socket.on('chat message',msg =>{
+        //chat history
+        chatHistory.push(msg);
+        chatHistory = chatHistoryTrim(chatHistory)
+        console.log(chatHistory);
+        //chat history
         //timestamp
         let today = new Date();
         let time = "<" + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ">  ";
+        msg = (time + msg);
         //timestamp
-        io.emit('chat message', (time + msg));
+        chatHistory.push(msg);
+        chatHistory = chatHistoryTrim(chatHistory)
+        console.log(chatHistory);
+        io.emit('chat message', (msg));
     })
     socket.on('disconnect', () =>{
         console.log('user disconnected')
